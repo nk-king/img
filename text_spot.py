4ad1ade5cff66f34141308bbba619e9cc397f6eb
nk-king <52429564+nk-king@users.noreply.github.com>
2020-04-27 21:18:37 +0530

    Update text_spot.py

--- text_spot.py
+++ text_spot.py
@@ -361,9 +361,9 @@ def main():
                 print('{:<70} {:<15} {:<15} {:<15} {:<10}'.format(layer, stats['layer_type'], stats['exec_type'],
                                                                   stats['status'], stats['real_time']))
 		
-        print("Done")
-	    print(f"frame shape {frame.shape}")
-	    cv2.imwrite("new_frame.jpg", frame)
+        cv2.imwrite("new_frame.jpg", frame)#print("Done")
+	    #print(f"frame shape {frame.shape}")
+	    #cv2.imwrite("new_frame.jpg", frame)
     
 if __name__ == '__main__':
     sys.exit(main() or 0)
